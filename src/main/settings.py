4a6bf528796a12b2443694da97832e158ab6160e
kozlovmv <mikhail.kozlov@eventplatform.ru>
2024-05-04 13:02:09 +0300

    Actualize config

--- src/main/settings.py
+++ src/main/settings.py
@@ -78,6 +78,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
         "HOST": os.environ["POSTGRES_HOST"],
         "PORT": os.environ["POSTGRES_PORT"],
         "PASSWORD": os.environ["POSTGRES_PASSWORD"],
